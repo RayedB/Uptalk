109e959490c4937b0fe55136e1b879f82e4404e3
RayedB <rayed.benbrahim@gmail.com>
2017-01-07 15:26:38 +0100

    rank posts

--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,21 @@ app.controller('MainCtrl', [
 '$scope',
 function($scope){
   $scope.test = 'Hello Mike!';
+
+  $scope.posts = [
+  	{ title: 'Post 1', content: 'lorem ipsum sin dolor matel', upvotes: 5},
+  	{ title: 'Post 2', content: 'lorem ipsum sin dolor matel', upvotes: 15},
+  	{ title: 'Post 3', content: 'lorem ipsum sin dolor matel', upvotes: 11},
+  	{ title: 'Post 4', content: 'lorem ipsum sin dolor matel', upvotes: 7},
+  	{ title: 'Post 5', content: 'lorem ipsum sin dolor matel', upvotes: 4},
+  ]
+
+  $scope.addPost = function() {
+  	$scope.posts.push({title:'Post '+( $scope.posts.length + 1), content: 'lorem ipsum sin dolor matel', upvotes: 0})
+  }
+
+  $scope.upVote = function(post) {
+  	$scope.upvotes ++;
+  }
+
 }]);
